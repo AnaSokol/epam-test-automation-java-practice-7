250f4b1aa73b484bf8f720c574df7aeeed7291e6
HvqEMAOU <hvqemaou@email.com>
2021-04-29 19:52:32 +0000

    Update Deposit.java

--- src/main/java/com/epam/test/automation/java/practice7/Deposit.java
+++ src/main/java/com/epam/test/automation/java/practice7/Deposit.java
@@ -1,10 +1,23 @@
 package com.epam.test.automation.java.practice7;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class Deposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+
+public abstract class Deposit {
+    private BigDecimal amount;
+    private int period;
+
+    public Deposit (BigDecimal depositAmount,int depositPeriod){
+        amount = depositAmount;
+        period = depositPeriod;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public abstract BigDecimal income ();
 }
