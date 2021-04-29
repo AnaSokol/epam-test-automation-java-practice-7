245ed32d47aad8bd7c9ef9b5429fcfd2c414c8ed
HvqEMAOU <hvqemaou@email.com>
2021-04-29 19:58:14 +0000

    Update SpecialDeposit.java

--- src/main/java/com/epam/test/automation/java/practice7/SpecialDeposit.java
+++ src/main/java/com/epam/test/automation/java/practice7/SpecialDeposit.java
@@ -1,10 +1,30 @@
 package com.epam.test.automation.java.practice7;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class SpecialDeposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class SpecialDeposit extends Deposit{
+
+    public SpecialDeposit(BigDecimal amount, int period) {
+        super(amount, period);
+    }
+
+    @Override
+    public BigDecimal income() {
+        BigDecimal monthlyIncome;
+        BigDecimal depositedSum = super.getAmount();
+        int period = super.getPeriod();
+        BigDecimal baseAmount = depositedSum;
+        BigDecimal endSum = depositedSum;
+        BigDecimal bd100 = new BigDecimal(String.valueOf(100));
+        for (int i = 0; i < period; i++){
+            BigDecimal interest = new BigDecimal(String.valueOf(i+1)).divide(bd100);
+            monthlyIncome = (baseAmount.multiply(interest)).setScale(2, RoundingMode.HALF_EVEN);
+            endSum = baseAmount.add(monthlyIncome);
+            baseAmount = endSum;
+        }
+        BigDecimal totalIncome = endSum.subtract(depositedSum);
+        return totalIncome;
+    }
+
 }
