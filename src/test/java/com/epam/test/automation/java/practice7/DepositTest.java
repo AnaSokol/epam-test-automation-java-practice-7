a5b163c5d0c0d68d472931588c0d156c0b13aaa8
HvqEMAOU <hvqemaou@email.com>
2021-04-30 11:26:00 +0000

    Update DepositTest.java

--- src/test/java/com/epam/test/automation/java/practice7/DepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice7/DepositTest.java
@@ -1,4 +1,29 @@
 package com.epam.test.automation.java.practice7;
 
+import org.testng.annotations.Test;
+import java.math.BigDecimal;
+import static org.testng.Assert.*;
+
 public class DepositTest {
+    BigDecimal depositAmount = new BigDecimal(String.valueOf(1000));
+    int depositPeriod = 2;
+    BaseDeposit dep1 = new BaseDeposit(depositAmount, depositPeriod);
+
+    @Test
+    public void testGetAmount() {
+         BigDecimal result = dep1.getAmount();
+         BigDecimal expected = depositAmount;
+         assertEquals(result, expected);
+    }
+
+    @Test
+    public void testGetPeriod() {
+        int result = dep1.getPeriod();
+        int expected = depositPeriod;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testIncome() {
+    }
 }
