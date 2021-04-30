be2be7e46b93045d73a3dcdf2b00e393286cafa2
HvqEMAOU <hvqemaou@email.com>
2021-04-30 11:10:04 +0000

    Update BaseDepositTest.java

--- src/test/java/com/epam/test/automation/java/practice7/BaseDepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice7/BaseDepositTest.java
@@ -1,4 +1,36 @@
 package com.epam.test.automation.java.practice7;
 
+import org.testng.annotations.Test;
+import java.math.BigDecimal;
+import static org.testng.Assert.*;
+
 public class BaseDepositTest {
+
+    @org.testng.annotations.Test
+    public void testIncome1() {
+        BigDecimal baseAmount = new BigDecimal("1000");
+        int period = 1;
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, period);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("50.00");
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome2() {
+        BigDecimal baseAmount = new BigDecimal("1000");
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, 2);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("102.50");
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome3() {
+        BigDecimal baseAmount = new BigDecimal("1000");
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, 3);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("157.62");
+        assertEquals(income, expected);
+    }
 }
