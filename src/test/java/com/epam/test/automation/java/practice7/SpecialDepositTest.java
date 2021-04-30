b27523d1b8a6682abc2b0e494850c94b064eedee
HvqEMAOU <hvqemaou@email.com>
2021-04-30 10:53:24 +0000

    Update SpecialDepositTest.java

--- src/test/java/com/epam/test/automation/java/practice7/SpecialDepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice7/SpecialDepositTest.java
@@ -1,4 +1,35 @@
 package com.epam.test.automation.java.practice7;
 
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.testng.Assert.*;
+
 public class SpecialDepositTest {
+    BigDecimal baseAmount = new BigDecimal("1000.00");
+
+    @Test
+    public void testIncome1() {
+        BigDecimal expected = new BigDecimal("10.00");
+        SpecialDeposit spDep = new SpecialDeposit (baseAmount, 1);
+        BigDecimal income = spDep.income();
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome2() {
+        BigDecimal expected = new BigDecimal("30.20");
+        SpecialDeposit spDep = new SpecialDeposit (baseAmount, 2);
+        BigDecimal income = spDep.income();
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome3() {
+        BigDecimal expected = new BigDecimal("61.11");
+        SpecialDeposit spDep = new SpecialDeposit (baseAmount, 3);
+        BigDecimal income = spDep.income();
+        assertEquals(income, expected);
+    }
 }
